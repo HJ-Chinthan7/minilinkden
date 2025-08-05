673ddd17dc5a13da827c69fed263bd8ca00d3a25
HJ-Chinthan7 <chinthanhj107@gmail.com>
2025-08-05 14:09:33 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); // ✅ <--- ADD THIS LINE
+const cors = require('cors'); 
 const connectDB = require('./utils/db');
 
 const app = express();
 dotenv.config();
 
-// CORS configuration
+
 app.use(cors({
   origin: 'https://magical-melomakarona-51c068.netlify.app',
   credentials: true
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// Routes
+
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
 const userRoutes = require('./routes/users');
@@ -24,7 +24,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 
-// Connect DB and start server
 connectDB().then(() => {
   console.log('MongoDB connected');
   app.listen(process.env.PORT || 5000, () => console.log('Server started on port 5000'));
